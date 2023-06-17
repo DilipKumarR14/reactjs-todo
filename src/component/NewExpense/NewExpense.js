0ea6b872762393ee3789c2ac54981597f6c8e150
Dilip Kumar R <dilipkumar14inc@gmail.com>
2023-06-17 18:13:07 +0530

    [Feature]Hide/UnHide the Add Expense Item

--- src/component/NewExpense/NewExpense.js
+++ src/component/NewExpense/NewExpense.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEdit, setIsEdit] = useState(false);
 
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     // console.log(expenseData);
     props.onAddExp(expenseData);
+    setIsEdit(false);
   };
 
+  const editExpenseDataHandler = () => {
+    setIsEdit(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEdit(false);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEdit ? (
+        <button onClick={editExpenseDataHandler}>Add New Expense</button>
+      ) : (
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}
+        onCancel={stopEditingHandler} />
+      )}
+
+      {/* {!isEdit &&  <button onClick={editExpenseDataHandler}>Add New Expense</button>}
+      {isEdit &&   <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />} */}
     </div>
   );
 };
